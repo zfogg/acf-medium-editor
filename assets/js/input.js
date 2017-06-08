59a1088e998d685d5d175972d56c3727861601c3
Zach Fogg <zach.fogg@gmail.com>
2017-06-08 03:35:37 -0700

    3.0.2

--- assets/js/input.js
+++ assets/js/input.js
@@ -28,30 +28,32 @@ var acf_medium_editor_timeout = false;
 
   function initPicker(element) {
       $(element).spectrum({
-    allowEmpty: false,
-    color: "#f00",
-    showInput: true,
-    showAlpha: false,
-    showPalette: true,
-    showInitial: true,
-    hideAfterPaletteSelect: false,
-    preferredFormat: "hex3",
-    change: function(color) {
-        setColor(color);
-    },
-    hide: function(color) {
-        applyColor(color);
-    },
-    palette: [
-        ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
-        ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
-        ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
-        ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
-        ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
-        ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
-        ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
-        ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
-    ]
+        color:                '#fff',
+        flat:                 true,
+        showInput:            true,
+        showInitial:          true,
+        allowEmpty:           true,
+        showAlpha:            false,
+        disabled:             false,
+        localStorageKey:      'medium_editor_spectrum',
+        showPalette:          false,
+        showPaletteOnly:      false,
+        togglePaletteOnly:    false,
+        showSelectionPalette: false,
+        clickoutFiresChange:  true,
+        preferredFormat:      'hex',
+        //cancelText: string,
+        //chooseText: string,
+        //togglePaletteMoreText: string,
+        //togglePaletteLessText: string,
+        //containerClassName: string,
+        //replacerClassName: string,
+        //maxSelectionSize: int,
+        //palette: [[string]],
+        //selectionPalette: [string]
+
+        change: function(color) { setColor(color); },
+        hide:   function(color) { setColor(color); },
       });
   }
 
